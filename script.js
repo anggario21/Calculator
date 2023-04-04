e601116f0f572b777ce2d07558c84a9fe0b409d0
anggario21d <anggario_21@apps.ipb.ac.id>
2023-04-04 17:01:25 +0700

    add js

---
+++ script.js
@@ -0,0 +1,108 @@
+const calculatorScreen = document.querySelector(".calculator-screen");
+const numbers = document.querySelectorAll(".number");
+let prevNumber = "";
+let calculationOperator = "";
+let currentNumber = "0";
+const operators = document.querySelectorAll(".operator");
+const equalSign = document.querySelector(".equal-sign");
+const clearBtn = document.querySelector(".all-clear");
+const decimal = document.querySelector(".decimal");
+const percentage = document.querySelector(".percentage");
+
+const updateScreen = (number) => {
+  calculatorScreen.value = number;
+};
+
+numbers.forEach((number) => {
+  number.addEventListener("click", () => {
+    updateScreen(event.target.value);
+  });
+});
+
+const inputNumber = (number) => {
+  if (currentNumber === "0") {
+    currentNumber = number;
+  } else {
+    currentNumber += number;
+  }
+};
+
+numbers.forEach((number) => {
+  number.addEventListener("click", (event) => {
+    inputNumber(event.target.value);
+    updateScreen(currentNumber);
+  });
+});
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", (event) => {
+    inputOperator(event.target.value);
+  });
+});
+
+const inputOperator = (operator) => {
+  if (calculationOperator === "") {
+    prevNumber = currentNumber;
+  }
+  calculationOperator = operator;
+  currentNumber = "";
+};
+
+const calculate = () => {
+  let result = "";
+  switch (calculationOperator) {
+    case "+":
+      result = parseFloat(prevNumber) + parseFloat(currentNumber);
+      break;
+    case "-":
+      result = parseFloat(prevNumber) - parseFloat(currentNumber);
+      break;
+    case "*":
+      result = parseFloat(prevNumber) * parseFloat(currentNumber);
+      break;
+    case "/":
+      result = parseFloat(prevNumber) / parseFloat(currentNumber);
+      break;
+    default:
+      return;
+  }
+  currentNumber = result;
+  calculationOperator = "";
+};
+
+equalSign.addEventListener("click", (event) => {
+  calculate();
+  updateScreen(currentNumber);
+});
+
+const clearAll = () => {
+  prevNumber = "";
+  calculationOperator = "";
+  currentNumber = "0";
+};
+
+clearBtn.addEventListener("click", (event) => {
+  clearAll();
+  updateScreen(currentNumber);
+});
+
+inputDecimal = (dot) => {
+  if (currentNumber.includes(".")) {
+    return;
+  }
+  currentNumber += dot;
+};
+
+decimal.addEventListener("click", (event) => {
+  inputDecimal(event.target.value);
+  updateScreen(currentNumber);
+});
+
+percentage.addEventListener("click", (event) => {
+  if (calculationOperator === "+" || calculationOperator === "-") {
+    currentNumber = (prevNumber * currentNumber) / 100;
+  } else {
+    currentNumber /= 100;
+  }
+  updateScreen(currentNumber);
+});
